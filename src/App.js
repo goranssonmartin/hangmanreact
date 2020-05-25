65ed0858a897a7ce23a8fe7fec30b3bbba0f10c5
Martin <martin.goransson94@gmail.com>
2020-05-25 16:00:56 +0200

    Added images and text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import hangmanImage from "./images/hangmanImage.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hänga gubbe</h1>
+      <p>
+        Gissa vilket ord det är med hjälp av bokstäverna nedan. Du har åtta
+        gissningar
+      </p>
+      <img src={hangmanImage} alt="Hangman" id="hangmanImage"></img>
     </div>
   );
 }
