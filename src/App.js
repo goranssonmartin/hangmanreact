3cfdf3944558e984044325dcebcd38471c3610f7
Martin <martin.goransson94@gmail.com>
2020-05-25 19:00:04 +0200

    Added buttons and guess text

--- src/App.js
+++ src/App.js
@@ -2,15 +2,39 @@ import React from "react";
 import "./App.css";
 import hangmanImage from "./images/hangmanImage.jpg";
 
+const words = ["hej", "hejsan", "hallå"];
+var charArray = () => {
+  return words[Math.floor(Math.random() * 3)].split("");
+};
+
+var correctWord = () => {
+  var currentWord = "";
+  var arrayOfCharacters = charArray();
+  currentWord = arrayOfCharacters.map((elem) => (currentWord += elem));
+  return currentWord[currentWord.length - 1];
+};
+console.log(correctWord());
+
+var letterCount = charArray().map((letter, index) => (
+  <p className="letterLine" key={index}>
+    _
+  </p>
+));
+var numberOfGuesses=0;
+
 function App() {
   return (
     <div className="App">
       <h1>Hänga gubbe</h1>
-      <p>
+      <p className="instructionText">
         Gissa vilket ord det är med hjälp av bokstäverna nedan. Du har åtta
         gissningar
       </p>
       <img src={hangmanImage} alt="Hangman" id="hangmanImage"></img>
+      <br />
+      {letterCount}
+      <p className="instructionText">Antal gissningar: {numberOfGuesses}</p>
+      <button className="letterButton">a</button>
     </div>
   );
 }
