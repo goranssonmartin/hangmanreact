ab611ef9fc3a8887d4da9926280d1160366bacad
Martin <martin.goransson94@gmail.com>
2020-06-09 09:23:40 +0200

    Added images and state for word guessing

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,39 @@
 import React, { useState } from "react";
 import "../Styling/App.css";
-import hangmanImage from "../images/hangmanImage.jpg";
+import hangmanStage1 from "../images/hangmanStage1.jpg";
+import hangmanStage2 from "../images/hangmanStage2.jpg";
+import hangmanStage3 from "../images/hangmanStage3.jpg";
+import hangmanStage4 from "../images/hangmanStage4.jpg";
+import hangmanStage5 from "../images/hangmanStage5.jpg";
+import hangmanStage6 from "../images/hangmanStage6.jpg";
+import hangmanStage7 from "../images/hangmanStage7.jpg";
+
 import { charArray } from "../words.js";
 import Button from "./Button";
 
 function App() {
   const alphabetLetters = "abcdefghijklmnopqrstuvwxyzåäö".split("");
   const disabledButton = alphabetLetters.map((letter) => false);
+  const wordLength = () => {
+    let arrayOfValues = [];
+    randomWord.forEach((elem) => arrayOfValues.push("_"));
+    return arrayOfValues;
+  };
+  const images = [
+    hangmanStage1,
+    hangmanStage2,
+    hangmanStage3,
+    hangmanStage4,
+    hangmanStage5,
+    hangmanStage6,
+    hangmanStage7,
+  ];
 
   const [disabledButtons, updateButton] = useState(disabledButton);
   const [randomWord, updateWord] = useState(charArray());
   const [numberOfGuesses, updateGuesses] = useState(0);
+  const [currentImage, updateImage] = useState(images[0]);
+  const [foundLetters, updateFoundLetters] = useState(wordLength());
 
   const buttonForEachLetter = alphabetLetters.map((letter, index) => {
     return (
@@ -23,30 +46,47 @@ function App() {
       />
     );
   });
-
-  let letterCount = randomWord.map((letter, index) => (
-    <p className="letterLine" key={index}>
-      _
-    </p>
-  ));
+  const checkCorrectGuess = (index) => {
+    const letterToCheck = alphabetLetters[index];
+    let currentWord = [...foundLetters];
+    let swapped = false;
+    randomWord.forEach((letterInWord, index) => {
+      if (letterInWord === letterToCheck) {
+        currentWord[index] = letterInWord;
+        swapped = true;
+      }
+    });
+    if (swapped) {
+      updateFoundLetters(currentWord);
+      return swapped;
+    }
+    return swapped;
+  };
 
   const onClickedLetter = (index) => {
     let newArray = [...disabledButtons];
     newArray[index] = true;
     updateButton(newArray);
-    let currentGuess = numberOfGuesses+1;
-    updateGuesses(currentGuess);
+    if (!checkCorrectGuess(index)) {
+      let currentGuess = numberOfGuesses + 1;
+      updateGuesses(currentGuess);
+      updateImage(images[currentGuess]);
+      if (currentGuess === 6) {
+        document.getElementById("letterButtonDiv").style.display = "none";
+        document.getElementById("youLostDiv").style.display = "block";
+      }
+    }
   };
 
   const onResetGame = () => {
     updateGuesses(0);
     updateWord(charArray());
     updateButton(disabledButton);
-    letterCount = randomWord.map((letter, index) => (
-      <p className="letterLine" key={index}>
-        _
-      </p>
-    ));
+    updateImage(images[0]);
+    updateFoundLetters(wordLength());
+    document.getElementById("letterButtonDiv").style.display = "block";
+    document.getElementById("youLostDiv").style.display = "none";
+    document.getElementById("youWonDiv").style.display = "none";
   };
 
   return (
@@ -55,11 +95,21 @@ function App() {
       <p className="instructionText">
         Gissa vilket det gömda ordet är med hjälp av bokstäverna nedan.
       </p>
-      <img src={hangmanImage} alt="Hangman" id="hangmanImage"></img>
+      <img src={currentImage} alt="Hangman" id="hangmanImage"></img>
       <br />
-      {letterCount}
+      {foundLetters.map((letter, index) => (
+        <p className="letterLine" key={index}>
+          {letter}
+        </p>
+      ))}
       <p className="instructionText">Antal gissningar: {numberOfGuesses}</p>
       <div id="letterButtonDiv">{buttonForEachLetter}</div>
+      <div id="youLostDiv">
+        <p>You Lost</p>
+      </div>
+      <div id="youWonDiv">
+        <p>You Won</p>
+      </div>
       <br />
       <Button
         onClickFunction={onResetGame}
